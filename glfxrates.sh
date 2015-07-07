bae242a06c1eeec0626ed98373da17bb272d789a
Craig Roush <crroush@gmail.com>
2015-07-06 22:21:54 -0600

    made it work from the command line without user needing to prompt it

--- glfxrates.sh
+++ glfxrates.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
 echo ===========================
 echo Welcome to GL FX Rate Fixer
-echo Enter .out file name:
-read filename </dev/tty
+# lame real bash scripts we like to pass in a file on the command line
+#echo Enter .out file name:
+#read filename </dev/tty
 echo 
 echo 
-
+# lets just set it to the first line off of the commandline 
+filename=$1
 # == VARIABLES ==
 newfile="NEW-"$filename
 truetrue=TRUE\,TRUE
